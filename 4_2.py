b90f73698ef2da37be46ce8274c22f8d3fe4e466
Mitchell Grice <mitch@gricey.net>
2017-12-04 15:10:07 +1000

    Day 4

---
+++ 4_2.py
@@ -0,0 +1,10 @@
+"""
+http://adventofcode.com/2017/day/4
+"""
+
+with open('4.in', 'r') as f:
+    data = [l.strip().split(' ') for l in f]
+
+data = [[''.join(sorted(w)) for w in l] for l in data]
+res = sum(1 for l in data if len(l) == len(set(l)))
+print(str(res))
