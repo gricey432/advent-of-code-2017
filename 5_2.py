e6c5b258e23719c2bcb6ecc44d12b436a592f8ee
Mitchell Grice <mitch@gricey.net>
2017-12-05 15:05:53 +1000

    Day 5

---
+++ 5_2.py
@@ -0,0 +1,21 @@
+"""
+http://adventofcode.com/2017/day/5
+"""
+
+with open('5.in', 'r') as f:
+    instructions = [int(l.strip()) for l in f]
+
+counter = 0
+cursor = 0
+while True:
+    instr = instructions[cursor]
+    if instr >= 3:
+        instructions[cursor] -= 1
+    else:
+        instructions[cursor] += 1
+    cursor += instr
+    counter += 1
+    if cursor < 0 or cursor >= len(instructions):
+        break
+
+print(str(counter))
